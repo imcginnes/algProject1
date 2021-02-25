38a8965d92ff2e71c1a6e8c7f520129303b69035
Joshua Barber <58489546+jbarberrrr@users.noreply.github.com>
2021-02-24 23:41:48 -0600

    Add files via upload

--- algProject1/backEnd.cpp
+++ algProject1/backEnd.cpp
@@ -1,26 +1,49 @@
 #include <iostream>
 #include <cmath>
 #include <ctime>
+#include <random>
 
 using namespace std;
 
-double genrndnum(double lower_limit, double upper_limit)
+int genRand()
 {
-    srand(time(NULL));
-	double rdnum = 0;
+	srand(time(NULL));
+	int num = 0;
 
-    rdnum = lower_limit + (rand() % (double)(upper_limit - lower_limit + 1));
+	do {
+		num = rand() & 100;
+	} while (num != 1);              //isPrime(num) == false);
 
-    return rdnum;
+	return num;
 }
 
+
+bool isPrime(int num)
+{
+	int i = 0;
+	bool prime = true;
+	
+	if (num == 0 || num == 1) {
+		prime = false;
+	}
+	else {
+		for (i = 2; i <= num / 2; ++i) {
+			if (num % i == 0) {
+				prime = false;
+				break;
+			}
+		}
+	}
+
+	return prime;
+}
+
+/*
 double gcd(double num1, double num2)
 {
-	/*
-
+	
 	Returns the Greatest Common Divisor (GCD) of two given numbers.
 
-*/
 
 
 	if (num1 == 0 && num2 == 0)
@@ -52,25 +75,25 @@ double gcd(double num1, double num2)
 
 void keygen(double& e_key, double& d_key, double phival)
 {
-    //Code to find the encryption key.
+	//Code to find the encryption key.
 
-    e_key = genrndnum(2, (phival));
+	e_key = genrndnum(2, (phival));
 
-    while (gcd(e_key, phival) != 1)
-    {
-        e_key = genrndnum(2, (phival - 1));
-    }
+	while (gcd(e_key, phival) != 1)
+	{
+		e_key = genrndnum(2, (phival - 1));
+	}
 
-    //Code to find decryption key.
+	//Code to find decryption key.
 
 	double k = 0;
-    d_key = (1 + (k * phival)) / e_key;
+	d_key = (1 + (k * phival)) / e_key;
 
-    while (((1 + (k * phival)) % e_key) != 0)
-    {
-        ++k;
-        d_key = (1 + (k * phival)) / e_key;
-    }
+	while (((1 + (k * phival)) % e_key) != 0)
+	{
+		++k;
+		d_key = (1 + (k * phival)) / e_key;
+	}
 
-    return;
-}
+	return;
+}*/
